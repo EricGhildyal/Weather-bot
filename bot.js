970217ee250c800ad2e053feda570341c2a5a2de
Eric Ghildyal <ericghildyal@gmail.com>
2017-01-26 18:31:38 -0500

    Api call

--- bot.js
+++ bot.js
@@ -2,10 +2,11 @@ var HTTPS = require('https');
 var request = require('request');
 
 var botID = process.env.BOT_ID;
+var apiKey = process.env.API_KEY;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/weather .*/g;
+      botRegex = /^\/weather*/g;
 
   if(request.text && botRegex.test(request.text)) {
     var city = request.text.replace(/\/weather/g, ' ');
@@ -15,12 +16,12 @@ function respond() {
       return "Default city is Pittsburgh \n Use /weather [city] for other cities \n More features to come!";
     }
 
-    if(city == " "){
+    if(city == ""){ //if no city given, default to PGH
       console.log("Defaulted to PGH!!!!");
       //default to pittsburgh
     }
 
-    processWeather(city, function(response){
+    processWeather(city, function(response){ //all other cities, process
       postMessage(response);
     });
     this.res.end();
@@ -32,9 +33,7 @@ function respond() {
 }
 
 // api call: http://api.openweathermap.org/data/2.5/weather?id=cityCode&units=imperial&appid=apiKey
-function processWeather(city, callback){
-  var dat = null;
-
+function processWeather(city, callback){ //callback is to send the message
   var cityCode = -1;
   cityCode = 1283240; //Kathmandu example
   //check for city code in file
@@ -43,25 +42,24 @@ function processWeather(city, callback){
     getWeather(cityCode, function(dat){ //cal api, wait for callback
       console.log("dat: " + dat);
       if(dat != null){
-        callback("The weather for " + dat.name + " is " + dat.main.temp);
+        callback("The weather for " + dat.name + " is " + dat.main.temp + "F"); //form the full message to be sent
       }else{
-        callback("Nothing was returned"); //"There was an unspecified error"
+        callback("Nothing Found :(");
       }
     });
   }else{
-    callback("I didn't understand that :("); //default response
+    callback("I don't know what " + city + " is..."); //default response
   }
 }
-
+ //function to call openwaethermap API, callback to processWeather
 function getWeather(cityCode, callback){
-  var url = "http://api.openweathermap.org/data/2.5/weather?id=" + cityCode + "&units=imperial&appid=aa18b5edfa68b9272ef1cd13f4602abe";
+  var url = "http://api.openweathermap.org/data/2.5/weather?id=" + cityCode + "&units=imperial&appid="+ apiKey;
   request({
   url: url,
   json: true
   }, function (error, response, body) {
     if (!error) {
-      console.log("body: " + body.main.temp);
-      callback(body);
+      callback(body); //send full JSON back
     }else{
       console.log("Error " + response.statusCode);
     }
