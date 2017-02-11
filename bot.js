ff9f638d0565dce75365f2bb3cc76aa74ef8a58d
Eric Ghildyal <ericghildyal@gmail.com>
2017-02-10 23:42:20 -0500

    fixing weather info

--- bot.js
+++ bot.js
@@ -66,7 +66,7 @@ function processWeather(city, callback){ //callback is to send the message
         function rainOrSnow(dat){
           var resp = "";
 
-          console.log(stringify(dat.rain));
+          console.log(JSON.stringify(dat.rain));
           // console.log(dat.snow[0]);
           if(dat.rain != undefined && dat.rain[0] >= 0){
             resp += "raining and";
