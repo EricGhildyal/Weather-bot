6abef045cc41881df8827a44a8f46b7a5a40a8d5
Eric Ghildyal <ericghildyal@gmail.com>
2017-02-11 00:13:36 -0500

    fixing weather data (hacky)

--- bot.js
+++ bot.js
@@ -66,7 +66,7 @@ function processWeather(city, callback){ //callback is to send the message
         function rainOrSnow(dat){
           var resp = "";
 
-          console.log(JSON.parse(dat.rain, (key, value) => {return value;};));
+          console.log(JSON.parse(JSON.stringify(dat.rain)).pop());
           // console.log(dat.snow[0]);
           if(dat.rain != undefined && dat.rain[0] >= 0){
             resp += "raining and";
